43a805ec08d546f9584abbfb3be2afd7bcdebda9
lukman2024 <rukumang.com@gmail.com>
2024-03-01 11:37:43 +0700

    Belajar reactJS (event handler)

--- my-react-app/src/components/Elements/Button/index.jsx
+++ my-react-app/src/components/Elements/Button/index.jsx
@@ -1,8 +1,13 @@
 const Button = (props) => {
-  const { children, classname = "bg-black" } = props;
+  const { children, classname = "bg-black", onClick = () => {}, type = Button } = props;
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} type="submit">
-      {props.children}
+    <button
+      className={`h-10 px-6 font-semibold rounded-md 
+    ${classname} text-white`}
+      type="type"
+      onClick={() => onClick()}
+    >
+      {children}
     </button>
   );
 };
--- my-react-app/src/components/Fragments/FromLogin.jsx
+++ my-react-app/src/components/Fragments/FromLogin.jsx
@@ -2,11 +2,19 @@ import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
+    window.location.href = "/products";
+  };
   return (
-    <form action="">
-      <InputForm label="email" type="email" placeholder="example@mail.com" nama="Email" />
-      <InputForm label="Password" type="Password" placeholder="******" nama="Password" />
-      <Button classname="bg-blue-600 w-full">login</Button>
+    <form onSubmit={handleLogin}>
+      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email" />
+      <InputForm label="Password" type="password" placeholder="******" name="password" />
+      <Button classname="bg-blue-600 w-full" type="submit">
+        Login
+      </Button>
     </form>
   );
 };
