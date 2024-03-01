2b216006200319b61caab72d0942425dd832b60f
lukman2024 <rukumang.com@gmail.com>
2024-03-01 09:11:22 +0700

    Belajar reactJS (rendering lists)

--- my-react-app/src/pages/product.jsx
+++ my-react-app/src/pages/product.jsx
@@ -1,19 +1,43 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CartProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "fixie Baru",
+    price: "freee!!",
+    image: "/images/product1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur 
+    adipisicing elit. Ad nihile`,
+  },
+  {
+    id: 2,
+    name: "fixie Baru",
+    price: "Bayar satt!!",
+    image: "/images/product1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur 
+  adipisicing elit.`,
+  },
+  {
+    id: 3,
+    name: "fixie Baru",
+    price: "Bayar satt!!",
+    image: "/images/product1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur 
+  adipisicing elit.`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/product1.jpg" />
-        <CardProduct.Body title="fixie new spect">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nihil quibusdam doloribus.</CardProduct.Body>
-        <CardProduct.Footer price="Free!!!" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/product1.jpg" />
-        <CardProduct.Body title="fixie new spect">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nihil quibusdam doloribus.</CardProduct.Body>
-        <CardProduct.Footer price="Free!!!" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
